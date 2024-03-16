9641a2cce5d47d1bdeff8cc4a965b058405b6546
JahongirOdilov <odilovjahongir2001@gmail.com>
2024-03-16 09:27:10 +0500

    portfolio

---
+++ main.js
@@ -0,0 +1,39 @@
+let options = {
+	strings: ["HELLO 👋", "I am", " Jahongir Odilov"],
+	typeSpeed: 50,
+	startDelay: 1000,
+	loop: true,
+	loopCount: Infinity,
+	backSpeed: 50,
+	backDelay: 400,
+	showCursor: true,
+	cursorChar: "",
+};
+
+let typed = new Typed("#sozchiqaradi", options);
+
+$(document).ready(function () {
+	$("#contactForm").submit(function (e) {
+		e.preventDefault(); // Prevent the form from submitting normally
+
+		// Serialize the form data
+		var formData = $(this).serialize();
+
+		// Send an AJAX request
+		$.ajax({
+			type: "POST",
+			url: "your_php_script.php", // Replace 'your_php_script.php' with the actual filename of your PHP script
+			data: formData,
+			success: function (response) {
+				// Display the response message
+				$("#msgSubmit").removeClass("hidden").html(response);
+			},
+			error: function (xhr, status, error) {
+				// Display an error message
+				$("#msgSubmit")
+					.removeClass("hidden")
+					.text("Oops! Something went wrong and we couldn't send your message.");
+			},
+		});
+	});
+});
